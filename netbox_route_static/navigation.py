7febb0678a4ac040131f863a9035fd08c71fc6fc
Wouter van der Vaart <wv@peakfactory.com>
2024-05-08 13:25:01 +0200

    v4.0 compat

--- netbox_route_static/navigation.py
+++ netbox_route_static/navigation.py
@@ -1,17 +1,15 @@
-from extras.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
-from utilities.choices import ButtonColorChoices
+from netbox.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 
 static = PluginMenuItem(
     link='plugins:netbox_route_static:staticroute_list',
     link_text='Static Route',
     permissions=['netbox_route_static.view_staticroute'],
     buttons=(
-        PluginMenuButton('plugins:netbox_route_static:staticroute_add', 'Add', 'mdi mdi-plus', ButtonColorChoices.GREEN),
+        PluginMenuButton('plugins:netbox_route_static:staticroute_add', 'Add', 'mdi mdi-plus'),
         PluginMenuButton(
             'plugins:netbox_route_static:staticroute_import',
             'Import',
-            'mdi mdi-upload',
-            ButtonColorChoices.CYAN
+            'mdi mdi-upload'
         ),
     )
 )
