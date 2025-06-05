a04a6ac833cf6cfc18ac940d0b6bb39fc1642bef
Wouter van der Vaart <wv@peakfactory.com>
2025-06-05 13:00:36 +0200

    FIXED API CREATE

--- netbox_route_static/api/nested_serializers/static.py
+++ netbox_route_static/api/nested_serializers/static.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from netbox.api.serializers import WritableNestedSerializer
 from netbox_route_static.models import StaticRoute
+from dcim.api.serializers import DeviceSerializer
+from ipam.api.serializers import VRFSerializer
 
 
 __all__ = (
@@ -11,7 +13,9 @@ __all__ = (
 
 class NestedStaticRouteSerializer(WritableNestedSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='plugins-api:netbox_route_static-api:staticroute-detail')
+    devices = DeviceSerializer(many=True, nested=True)
+    vrf = VRFSerializer(nested=True)
 
     class Meta:
         model = StaticRoute
-        fields = ('url', 'id', 'display', 'prefix', 'next_hop', 'name', 'metric', 'permanent', 'custom_fields')
+        fields = ('url', 'id', 'display', 'prefix', 'next_hop', 'name', 'metric', 'permanent', 'custom_fields', 'vrf', 'devices')
--- netbox_route_static/api/serializers/static.py
+++ netbox_route_static/api/serializers/static.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-
+from netbox.api.fields import SerializedPKRelatedField
+from dcim.models import Device
 from dcim.api.serializers import DeviceSerializer
 from ipam.api.serializers import VRFSerializer
 from netbox.api.serializers import NetBoxModelSerializer
@@ -13,8 +14,14 @@ __all__ = (
 
 class StaticRouteSerializer(NetBoxModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='plugins-api:netbox_route_static-api:staticroute-detail')
-    devices = DeviceSerializer(many=True, nested=True)
-    vrf = VRFSerializer(nested=True)
+    devices = SerializedPKRelatedField(
+        queryset=Device.objects.all(),
+        serializer=DeviceSerializer,
+        nested=True,
+        required=False,
+        many=True
+    )
+    vrf = VRFSerializer(nested=True, required=False, allow_null=True)
 
     class Meta:
         model = StaticRoute
