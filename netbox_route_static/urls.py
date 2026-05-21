4a52b236a80641d4425e00ac4a4791d2a01f1edc
Wouter van der Vaart <wv@peakfactory.com>
2026-05-21 11:10:42 +0200

    fix

--- netbox_route_static/urls.py
+++ netbox_route_static/urls.py
@@ -6,12 +6,8 @@ from . import views
 from .models import StaticRoute
 
 urlpatterns = [
-    path('routes/static/', views.StaticRouteListView.as_view(), name='staticroute_list'),
-    path('routes/static/add/', views.StaticRouteEditView.as_view(), name='staticroute_add'),
-    path('routes/static/import/', views.StaticRouteListView.as_view(), name='staticroute_import'),
-    path('routes/static/<int:pk>/', views.StaticRouteView.as_view(), name='staticroute'),
-    path('routes/static/<int:pk>/edit/', views.StaticRouteEditView.as_view(), name='staticroute_edit'),
-    path('routes/static/<int:pk>/devices/', views.StaticRouteDevicesView.as_view(), name='staticroute_devices'),
-    path('routes/static/<int:pk>/delete/', views.StaticRouteDeleteView.as_view(), name='staticroute_delete'),
-    path('routes/static/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='staticroute_changelog', kwargs={'model': StaticRoute}),
+    path(
+        'routes/static/', include(get_model_urls(app_name, 'staticroute', detail=False))
+    ),
+    path('routes/static/<int:pk>/', include(get_model_urls(app_name, 'staticroute'))),
 ]
